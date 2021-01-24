e8b40dd6b5d570cefd1fccd93449d20b077a3823
藤原未来 <www.motimoti12@gmail.com>
2021-01-24 23:06:08 +0900

    ログイン機能のフォームの作成 取得してきたemailとpasswordが一致すればセッションに格納する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:email])
+    # ユーザーのパスワードとあっていたら
+    if user && user.authenticate(params[:password])
+      # セッションにアクセスできる データを格納している
+      session[:user_id] = user.id
+      redirect_to user_path, notice: "ログインに成功しました。"
+    else
+      flash.now[:alert] = "メールアドレスもしくはパスワードが間違っています。"
+      render :new
+    end
+  end
 end
