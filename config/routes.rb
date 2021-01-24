bac0fab8e392b905a743a34a05f061b32283d5a7
藤原未来 <www.motimoti12@gmail.com>
2021-01-24 20:29:11 +0900

    homeコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  root 'homes#top'
+  resource :user, only: [:new,:create,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
