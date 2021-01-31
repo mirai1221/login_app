1e454739bf0056efbfd63a102118a8fa9d0e8a1d
藤原未来 <www.motimoti12@gmail.com>
2021-01-31 11:39:33 +0900

    already_login?メソッドをdestroyアクションは使わない

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :already_login?, except: :destroy
   def new
   end
 
@@ -6,12 +7,17 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     # ユーザーのパスワードとあっていたら
     if user && user.authenticate(params[:password])
-      # セッションにアクセスできる データを格納している
+      # セッションにアクセスできる データを格納している cookieに保存されている
       session[:user_id] = user.id
       redirect_to user_path, notice: "ログインに成功しました。"
     else
       flash.now[:alert] = "メールアドレスもしくはパスワードが間違っています。"
       render :new
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "ログアウトに成功しました。"
   end
 end
