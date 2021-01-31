9d2add1da9bdf048692e0124c4ed5c90ee9eaca0
藤原未来 <www.motimoti12@gmail.com>
2021-01-31 11:35:26 +0900

    current_userメソッドを追加 helper_metohdでviewでも使えるようにする ログインしているか確かめるメソッドを追加 ログインしている場合 ユーザーの詳細ページにリダイレクトさせる ログインしていない場合 ログインページにリダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user # ビューで使えるようにする
+
+  def login? # ログインしているか確かめるメソッド
+    if current_user.nil? # current_userがnilの時
+      redirect_to login_path, alert: "ログインしてください。"
+    end
+  end
+
+  def already_login?
+    unless current_user.nil?
+      redirect_to user_path, notice: "ログインしています。"
+    end
+  end
+
+  def current_user
+    if session[:user_id]
+      current_user ||= User.find(session[:user_id])
+    end
+  end
 end
